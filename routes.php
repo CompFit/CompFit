0d1a47e8954f119f380693cf98c94b5648eeeeee
eesharp <eesharp@smu.edu>
2016-03-22 22:40:13 -0500

    Endpoints - POST user, some team
    I added a post for user using the userPost.html. Probably need to
    change this based on what GUI does. I also added a delete to
    user/user_id that can be carried out with a command on console (curl –i
    –X DELETE http://zero-to-slim.dev/user/{user_id}). Works for me. I
    don't know what to return for this. Added some get methods for team.

--- routes.php
+++ routes.php
@@ -42,3 +42,90 @@ $app->get('/user/{user_id}',
     return $response->write('' . $strToReturn);
   }
 );
+$app->post('/user',
+  function ($request, $response, $args){
+    $db = $this->dbConn;
+    $strToReturn = '';
+
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+
+    $db->query('INSERT INTO users (first_name, last_name, username, email) VALUES ("'.$first_name.'", "'.$last_name.'", "'.$username.'", "'.$email.'")');
+
+    foreach($db->query('select * from users') as $row){
+      $strToReturn .= '<br /> user_id: ' . $row['user_id'] .' <br /> username: ' . $row['username'];
+      $strToReturn .= '<br /> first_name: ' . $row['first_name'] .' <br /> last_name: ' . $row['last_name'];
+      $strToReturn .= '<br />';
+    }
+
+    return $response->write('' . $strToReturn);
+  }
+);
+$app->delete('/user/{user_id}',
+  function ($request, $response, $args){
+    $db = $this->dbConn;
+    $strToReturn = '';
+    $user_id = $request->getAttribute('user_id');
+
+    foreach($db->query('select * from users where user_id = "'.$user_id.'"') as $row){
+      $strToReturn .= '<br /> user_id: ' . $row['user_id'] .' <br /> username: ' . $row['username'];
+      $strToReturn .= '<br /> first_name: ' . $row['first_name'] .' <br /> last_name: ' . $row['last_name'];
+    }
+
+    $db->query('DELETE FROM users WHERE user_id = "'.$user_id.'"');
+
+    return $response->write('Deleting <br />' . $strToReturn);
+  }
+);
+
+
+$app->get('/team',
+  function ($request, $response, $args){
+    $db = $this->dbConn;
+    $strToReturn = '';
+
+    foreach($db->query('select * from teams') as $row){
+      $strToReturn .= '<br /> team_id: ' . $row['team_id'] .' <br /> team_name: ' . $row['team_name'];
+      $strToReturn .= '<br /> captain_id: ' . $row['captain_id'] . '<br />';
+    }
+
+    return $response->write('' . $strToReturn);
+  }
+);
+
+$app->get('/team/{team_id}',
+  function ($request, $response, $args){
+    $db = $this->dbConn;
+    $strToReturn = '';
+    $team_id = $request->getAttribute('team_id');
+
+    foreach($db->query('select * from teams where team_id = "'.$team_id.'"') as $row){
+      $strToReturn .= '<br /> team_id: ' . $row['team_id'] .' <br /> team_name: ' . $row['team_name'];
+      $strToReturn .= '<br /> captain_id: ' . $row['captain_id'];
+    }
+
+    return $response->write('' . $strToReturn);
+  }
+);
+
+$app->get('/team/{team_id}/{captain_id}',
+  function ($request, $response, $args){
+    $db = $this->dbConn;
+    $strToReturn = '';
+    $team_id = $request->getAttribute('team_id');
+    $captain_id = $request->getAttribute('captain_id');
+
+    foreach($db->query('select * from teams where team_id = "'.$team_id.'"') as $row){
+      $strToReturn .= '<br /> team_id: ' . $row['team_id'] .' <br /> team_name: ' . $row['team_name'];
+      $strToReturn .= '<br /> captain_id: ' . $row['captain_id'];
+      foreach($db->query('select * from users where user_id = "'.$captain_id.'"') as $row){
+        $strToReturn .= ' <br /> username: ' . $row['username'];
+        $strToReturn .= '<br /> first_name: ' . $row['first_name'] .' <br /> last_name: ' . $row['last_name'];
+      }
+    }
+
+    return $response->write('' . $strToReturn);
+  }
+);
