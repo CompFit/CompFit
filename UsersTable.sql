876ce452caf5f98d4c616cf7290901913b502b39
estephanian <estephanian@users.noreply.github.com>
2016-03-24 00:48:42 -0500

    Update UsersTable.sql

--- UsersTable.sql
+++ UsersTable.sql
@@ -21,10 +21,15 @@ CREATE TABLE Users (
     PRIMARY KEY    (user_id)
 
 
+
+
+
+
 #Inserting data into my Categories table
 insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (2468, 'Jackson', 'Teller', 'jteller34', 'jacksteller@gmail.com', '94%$yT3v', '2016-18-03', '2016-22-03');
 insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (1938, 'Filip', 'Telford', 'fipsy888', 'ftelford@gmail.com', 'password1234', '2016-24-02', '2016-18-03');
-insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (5834, 'Comedy', 'Gemma', 'Teller', 'mommaofdacrew66', 'gteller@aol.com', '1a2b3c4d', '2016-10-03', '2016-20-03');
-insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (2835, 'Action', 'Alex', 'Trager', 'atrain48', 'alextrager@hotmail.com', '1234ABCD', '2016-12-03', '2016-15-03');
-insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (9388, 'Thriller', 'Clay', 'Morrow', 'bigdaddy99', 'claymorrow10@aol.com', 'mysonsandI22', '2016-27-02', '2016-20-03');
-insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (3454, 'Family', 'Tara', 'Knowles', 'tKnowles10', 'taraknowles@gmail.com', 'pa$$word', '2016-26-02', '2016-21-03');
+insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (5834, 'Gemma', 'Teller', 'mommaofdacrew66', 'gteller@aol.com', '1a2b3c4d', '2016-10-03', '2016-20-03');
+insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (2835, 'Alex', 'Trager', 'atrain48', 'alextrager@hotmail.com', '1234ABCD', '2016-12-03', '2016-15-03');
+insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (9388, 'Clay', 'Morrow', 'bigdaddy99', 'claymorrow10@aol.com', 'mysonsandI22', '2016-27-02', '2016-20-03');
+insert into Users (user_id, first_name, last_name, username, email, password, created_Timestamp, last_login) values (3454, 'Tara', 'Knowles', 'tKnowles10', 'taraknowles@gmail.com', 'pa$$word', '2016-26-02', '2016-21-03');
+
