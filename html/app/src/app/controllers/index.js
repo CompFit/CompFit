c0a365dea4053645b376cea8da5bddc4de5fb589
Ashley Pancho <apancho@smu.edu>
2016-04-06 12:32:00 -0500

    commiting to pull from master

--- html/app/src/app/controllers/index.js
+++ html/app/src/app/controllers/index.js
@@ -6,8 +6,7 @@ import AuthenticationController from './authentication';
 import HomeController from './home';
 import LoginController from './login';
 import MyProfileController from './myProfile';
-import Page1Controller from './page1';
-import Page2Controller from './page2';
+import AboutController from './about';
 import RegisterController from './register';
 import TeamController from './team';
 
@@ -18,8 +17,7 @@ export default angular
     .controller( 'HomeController', HomeController )
     .controller( 'LoginController', LoginController )
     .controller( 'MyProfileController', MyProfileController )
-    .controller( 'Page1Controller', Page1Controller )
-    .controller( 'Page2Controller', Page2Controller )
+    .controller( 'AboutController', AboutController )
     .controller( 'RegisterController', RegisterController )
     .controller( 'TeamController', TeamController )
     .name;
--- html/app/src/app/routes.js
+++ html/app/src/app/routes.js
@@ -4,8 +4,6 @@ import authenticationTemplate from 'controllers/authentication/template.html';
 import homeTemplate from 'controllers/home/template.html';
 import loginTemplate from 'controllers/login/template.html';
 import myProfileTemplate from 'controllers/myProfile/template.html';
-import page1Template from 'controllers/page1/template.html';
-import page2Template from 'controllers/page2/template.html';
 import registerTemplate from 'controllers/register/template.html';
 import teamTemplate from 'controllers/team/template.html';
 
@@ -43,16 +41,6 @@ export default function ( $stateProvider, $urlRouterProvider, $locationProvider 
             url: '/team/:id',
             templateUrl: teamTemplate,
             controller: 'TeamController'
-        } )
-        .state( 'app.page1', {
-            url: '/page1',
-            templateUrl: page1Template,
-            controller: 'Page1Controller'
-        } )
-        .state( 'app.page2', {
-            url: '/page2',
-            templateUrl: page2Template,
-            controller: 'Page2Controller'
         } )
         .state( 'app.login', {
             url: '/login',
