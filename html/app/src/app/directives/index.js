b35c765a8b5e12ecdac70597e2e32f4d48f0c164
Tsiems <tsiems@smu.edu>
2016-04-14 10:56:52 -0500

    Beginning of posting challenges

--- html/app/src/app/directives/index.js
+++ html/app/src/app/directives/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 
 import teams_sidebar from './teams_sidebar';
+import create_challenge_modal from './create_challenge_modal';
 import create_team_modal from './create_team_modal';
 import challenges_sidebar from './challenges_sidebar';
 import exercises_sidebar from './exercises_sidebar';
@@ -10,6 +11,7 @@ import logbutton from './logbutton';
 export default angular
     .module( 'app.directives', [] )
     .directive( 'seedSidebar', ['Teams', 'Users', teams_sidebar] )
+    .directive( 'createChallengeModal', ['Teams','Users','Challenges', '$timeout', create_challenge_modal] )
     .directive( 'createTeamModal', ['Teams','Users', '$timeout', create_team_modal] )
     .directive( 'challengesSidebar', ['Challenges', challenges_sidebar] )
     .directive( 'exercisesSidebar', ['Exercises', exercises_sidebar] )
